3885efb30cbfba26d5429850d6257823ef2c3dd5
Ludwig <ludde.oelker@gmail.com>
2023-11-01 16:45:36 +0100

    Readability changes: Moved toupper() into the main file and seperated the cin input with clear instructions

--- CoffeeAction.h
+++ CoffeeAction.h
@@ -4,17 +4,21 @@
 void changeInCoins();
 
 class CoffeeAction {
+    // Declaration of variables used in this scope
     std::string productName;
     double price;
     double payment;
     double change;
 
 public:
+    // The action of the machine which takes two parameters from the user
     CoffeeAction(double money, char product) {
 
-        switch (toupper(product)) {
+        // Switch case which handles the users choice of char which leads to the corresponding product
+        // and assigns price and productName the corresponding values to further process them
+        switch (product) {
             case 'C':
-                price = 8;
+                price = 8.00;
                 productName = "Coffee";
                 break;
             case 'E':
@@ -35,28 +39,34 @@ public:
                 break;
         }
 
+        // Reassigning variable to start the change process
         payment = money;
         change = payment - price;
 
-        if (change > 0) {
+        // Calls the change function if change is large or equal to 0
+        if (change >= 0) {
             changeInCoins();
         }
 
     }
 
     void changeInCoins() {
+        // Converts the total amount of change into coins to be received
         int total = (int) (change * 100);
         int ten = total / 1000;
         int five = (total % 1000) / 500;
         int one = ((total % 1000) % 500) / 100;
         int half = (((total % 1000) % 500) % 100) / 50;
         int rest = ((((total % 1000) % 500) % 100) % 50) % 50;
-        std::cout << "\tThis is your total change: " << change << "\n\t"
+        // Visual statement for the user to see what the total change is and a breakdown of coins to be received
+        std::cout << "\n\tThis is your total change: " << change << "\n\t"
                   << "and this is a breakdown of what coins you will receive\n\t"
                   << "-------------------------------------\n"
                   << "\tTen\tFive\tOne\tHalf\tRest\n\t"
                   << ten << "\t" << five << "\t" << one << "\t" << half << "\t" << rest << std::endl;
     }
+
+    // Getters & Setters
 
     const std::string &getProductName() const {
         return productName;
--- main.cpp
+++ main.cpp
@@ -5,25 +5,36 @@ using namespace std;
 
 int main() {
 
+    // Declaring variables used in the main file
     double userCash;
     string userChoice;
     char cleanUserChoice;
 
+    // Loop the machine until a valid action has been completed
     do {
 
-        cout << "\tPlease input cash and chose an option\n"
+        // Prints out the visual instructions of the machine
+        cout << "\n\tWelcome to the hot drinks dispensary!\n\n"
                 "\tC(offee)\t\t8.00kr\n"
                 "\tE(spresso)\t\t9.50kr\n"
                 "\tO(Chocholate)\t\t7.50kr\n"
                 "\tL(Caffe Latte)\t\t9.00kr\n"
                 "\tP(Cappuccino)\t\t9.50kr\n";
 
-        cin >> userCash >> userChoice;
+        // Asks for the users cash input and option
+        cout << "\n\tPlease input your cash and hit enter: ";
+        cin >> userCash;
 
-        cleanUserChoice = userChoice.at(0);
+        cout << "\n\tPlease input what option you would like and hit enter: ";
+        cin >> userChoice;
 
+        // Cleans the userChoice input to ensure only one char is read and converts it into upper case
+        cleanUserChoice = toupper(userChoice.at(0));
+
+        // Creates the CoffeeAction and fills the parameters with the users input
         CoffeeAction coffeeAction(userCash, cleanUserChoice);
 
+        // Error handling if the user did not enter enough money or an incorrect char
         if (coffeeAction.getChange() >= 0 && !coffeeAction.getProductName().empty()) {
             cout << "\n\tEnjoy your " << coffeeAction.getProductName() << "." << endl;
             break;
